d9842632e3a70ef27a86b20b60af058e7e88399c
iamabdullahmalik <malik.ab@hotmail.com>
2023-01-20 11:15:01 +0500

    cookies consent added

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { App } from './App';
 import { GlobalStyle } from './Components/GlobalStyle';
 import { fetchWorks } from './utils/getWorks';
 import './i18n';
+import CookieConsent, { Cookies } from "react-cookie-consent";
 
 await fetchWorks();
 
@@ -15,5 +16,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <GlobalStyle />
       <App />
     </HashRouter>
+    <CookieConsent location="bottom" cookieName="myAwesomeCookieName3" enableDeclineButton expires={999} overlay>
+      This website uses cookies to enhance the user experience. We'll assume you're with this, but you can opt-out if you wish.
+    </CookieConsent>
   </React.StrictMode>
 );
